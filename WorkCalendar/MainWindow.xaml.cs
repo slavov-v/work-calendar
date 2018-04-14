8b87f980be23056c824f23015af75c83bc00e38b
Vasil Slavov <v.slavov96@gmail.com>
2018-04-14 23:22:25 +0300

    Add task and meeting removing, refactor some things

--- WorkCalendar/MainWindow.xaml.cs
+++ WorkCalendar/MainWindow.xaml.cs
@@ -20,9 +20,9 @@ namespace WorkCalendar
     /// </summary>
     public partial class MainWindow : Window
     {
-        // TODO: Add Task and Meeting removing
         private List<Task> currentTasks;
         private List<Meeting> currentMeetings;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -30,14 +30,18 @@ namespace WorkCalendar
             populateLists();
         }
 
+        private DateTime GetDefaultDate()
+        {
+            return mainViewCalendar.SelectedDate.HasValue ? mainViewCalendar.SelectedDate.Value : DateTime.Now;
+        }
+
         private void populateLists()
         {
             listBox_Meetings.Items.Clear();
             listBox_Tasks.Items.Clear();
 
-            DateTime selectedDate = mainViewCalendar.SelectedDate.HasValue ? mainViewCalendar.SelectedDate.Value : DateTime.Now;
-            currentTasks = EventManager.GetTasksDueDate(selectedDate);
-            currentMeetings = EventManager.GetMeetingsOnDate(selectedDate);
+            currentTasks = EventManager.GetTasksDueDate(GetDefaultDate());
+            currentMeetings = EventManager.GetMeetingsOnDate(GetDefaultDate());
 
             foreach (Task item in currentTasks)
             {
@@ -61,6 +65,8 @@ namespace WorkCalendar
             listBox_Tasks.Visibility = Visibility.Hidden;
             listBox_Meetings.Visibility = Visibility.Hidden;
             btn_ConfirmAddMeeting.Visibility = Visibility.Hidden;
+            btn_RemoveMeeting.Visibility = Visibility.Hidden;
+            btn_RemoveTask.Visibility = Visibility.Hidden;
             btn_ConfirmAddTask.Visibility = Visibility.Visible;
             tb_Description.Visibility = Visibility.Visible;
             btn_Cancel.Visibility = Visibility.Visible;
@@ -75,6 +81,8 @@ namespace WorkCalendar
             listBox_Tasks.Visibility = Visibility.Hidden;
             listBox_Meetings.Visibility = Visibility.Hidden;
             btn_ConfirmAddTask.Visibility = Visibility.Hidden;
+            btn_RemoveMeeting.Visibility = Visibility.Hidden;
+            btn_RemoveTask.Visibility = Visibility.Hidden;
             btn_ConfirmAddMeeting.Visibility = Visibility.Visible;
             tb_Description.Visibility = Visibility.Visible;
             btn_Cancel.Visibility = Visibility.Visible;
@@ -92,17 +100,18 @@ namespace WorkCalendar
             btn_AddMeeting.Visibility = Visibility.Visible;
             listBox_Tasks.Visibility = Visibility.Visible;
             listBox_Meetings.Visibility = Visibility.Visible;
-            DateTime selectedDate = mainViewCalendar.SelectedDate.HasValue ? mainViewCalendar.SelectedDate.Value : DateTime.Now;
-            lb_Tasks.Content = string.Format("Tasks due {0}", selectedDate.Date.ToString("dd/MM/yyyy"));
-            lb_Meetings.Content = string.Format("Meetings on {0}", selectedDate.Date.ToString("dd/MM/yyyy"));
+            btn_RemoveMeeting.Visibility = Visibility.Visible;
+            btn_RemoveTask.Visibility = Visibility.Visible;
+            lb_Tasks.Content = string.Format("Tasks due {0}", GetDefaultDate().Date.ToString("dd/MM/yyyy"));
+            lb_Meetings.Content = string.Format("Meetings on {0}", GetDefaultDate().Date.ToString("dd/MM/yyyy"));
+            tb_Description.Text = string.Empty;
         }
 
         private void mainViewCalendar_OnSelectedDatesChanged(object sender, SelectionChangedEventArgs e)
         {
             populateLists();
-            DateTime selectedDate = mainViewCalendar.SelectedDate.HasValue ? mainViewCalendar.SelectedDate.Value : DateTime.Now;
-            lb_Tasks.Content = string.Format("Tasks due {0}", selectedDate.Date.ToString("dd/MM/yyyy"));
-            lb_Meetings.Content = string.Format("Meetings on {0}", selectedDate.Date.ToString("dd/MM/yyyy"));
+            lb_Tasks.Content = string.Format("Tasks due {0}", GetDefaultDate().Date.ToString("dd/MM/yyyy"));
+            lb_Meetings.Content = string.Format("Meetings on {0}", GetDefaultDate().Date.ToString("dd/MM/yyyy"));
         }
 
         private void btn_AddTask_Click(object sender, RoutedEventArgs e)
@@ -112,7 +121,13 @@ namespace WorkCalendar
 
         private void btn_ConfirmAddTask_Click(object sender, RoutedEventArgs e)
         {
-            Task newTask = new Task(tb_Description.Text, DateTime.Now, mainViewCalendar.SelectedDate.Value.Date);
+            if(tb_Description.Text.Length == 0)
+            {
+                MessageBox.Show("Description can not be empty!");
+                return;
+            }
+
+            Task newTask = new Task(tb_Description.Text, DateTime.Now, GetDefaultDate());
             newTask.Save();
             triggerGeneralMode();
             populateLists();
@@ -125,7 +140,14 @@ namespace WorkCalendar
 
         private void btn_ConfirmAddMeeting_Click(object sender, RoutedEventArgs e)
         {
-            Meeting newMeeting = new Meeting(tb_Description.Text, mainViewCalendar.SelectedDate.Value.Date);
+            if (tb_Description.Text.Length == 0)
+            {
+                MessageBox.Show("Description can not be empty!");
+                return;
+            }
+
+            
+            Meeting newMeeting = new Meeting(tb_Description.Text, GetDefaultDate());
             newMeeting.Save();
             triggerGeneralMode();
             populateLists();
@@ -134,6 +156,31 @@ namespace WorkCalendar
         private void btn_Cancel_Click(object sender, RoutedEventArgs e)
         {
             triggerGeneralMode();
+        }
+
+        private void btn_RemoveMeeting_Click(object sender, RoutedEventArgs e)
+        {
+            if(listBox_Meetings.SelectedIndex < 0)
+            {
+                MessageBox.Show("No Meeting has been selected!");
+                return;
+            }
+
+            EventManager.RemoveMeeting(currentMeetings[listBox_Meetings.SelectedIndex].Id);
+            populateLists();
+
+        }
+
+        private void btn_RemoveTask_Click(object sender, RoutedEventArgs e)
+        {
+            if(listBox_Tasks.SelectedIndex < 0)
+            {
+                MessageBox.Show("No Task has been selected!");
+                return;
+            }
+
+            EventManager.RemoveTask(currentTasks[listBox_Tasks.SelectedIndex].Id);
+            populateLists();
         }
     }
 }
